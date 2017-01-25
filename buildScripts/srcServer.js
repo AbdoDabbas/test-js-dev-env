7fff07392a56f6fe427d919c9ebaeca2cfca8c4b
Abdulfattah Dabbas <abdo.dabbas@gmail.com>
2017-01-25 13:56:41 +0300

    - finished linting

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev.js';
+
+/* eslint-disable no-console */
 
 const port = 3005;
 const app = express();
+
+const compiler = webpack(config);
+const webpackMiddleware = require('webpack-dev-middleware')(compiler,{
+  noInfo: true,
+  publicPath: config.output.publicPath
+});
+
+app.use(webpackMiddleware);
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname,'../src/index.html'));
